cc5768fbbf591bdeacad58b33315b7766fa618d4
Bram Stolk <b.stolk@gmail.com>
2018-10-23 14:00:03 -0700

    Be paranoid about the geometries.

--- ode/demo/demo_convex.cpp
+++ ode/demo/demo_convex.cpp
@@ -28,6 +28,9 @@
 #ifdef HAVE_UNISTD_H
 #	include <unistd.h>
 #endif
+#if defined(linux)
+#	include <fenv.h>
+#endif
 #include <ode/ode.h>
 #include <drawstuff/drawstuff.h>
 #include "texturepath.h"
@@ -81,6 +84,8 @@ static void nearCallback(void *data, dGeomID o1, dGeomID o2)
 	const int N = 32;
 	dContact contact[N];
 	int n = dCollide (o1,o2,N,&(contact[0].geom),sizeof(dContact));
+	if ( n==32 )
+		fprintf( stdout, "WARNING! collision exceeded 32 contacts.\n"  );
 	if (n > 0) 
 	{
 		for (int i=0; i<n; i++) 
@@ -151,6 +156,59 @@ static void command(int cmd)
 	}
 }
 
+static void draw_plane_normals( int nr )
+{
+	dGeomID geom = hgeom[nr];
+	dBodyID body = hbody[nr];
+	const dReal *pos = dGeomGetPosition(geom);
+	const dReal *rot = dGeomGetRotation(geom);
+	const int numf = halton_numf[nr];
+	const unsigned int* reader = halton_faces[nr];
+	const dReal* verts = halton_verts[nr];
+	for ( int f=0; f<numf; ++f )
+	{
+		const unsigned int sz = *reader++;
+		dsSetColor(0.0,0.0,0.4);
+		dVector3 xv0,xv1;
+		dVector3 c = {0,0,0};
+		for ( int j=0; j<sz-1; ++j )
+		{
+			const unsigned int i0 = reader[j+0];
+			const unsigned int i1 = reader[j+1];
+			const dReal* v0 = verts+3*i0;
+			const dReal* v1 = verts+3*i1;
+			//dsDrawLine(v0,v1);
+			dBodyGetRelPointPos(body, v0[0],v0[1],v0[2], xv0);
+			dBodyGetRelPointPos(body, v1[0],v1[1],v1[2], xv1);
+			dsDrawLine( xv0, xv1 );
+			if ( j==0 )
+				dAddVectors3( c,v0,c );
+			dAddVectors3( c,v1,c );
+		}
+		c[0] = c[0] / sz;
+		c[1] = c[1] / sz;
+		c[2] = c[2] / sz;
+		dVector3 xc;
+		dBodyGetRelPointPos(body, c[0],c[1],c[2], xc);
+
+		const dReal* plane = halton_planes[nr] + f*4;
+		const dReal w0 = plane[3];
+		const dReal w1 = w0 + 0.1;
+		dVector3 p0 = { plane[0]*w0, plane[1]*w0, plane[2]*w0 };
+		dVector3 p1 = { plane[0]*w1, plane[1]*w1, plane[2]*w1 };
+		dVector3 xp0, xp1;
+		dBodyGetRelPointPos(body, p0[0],p0[1],p0[2], xp0);
+		dBodyGetRelPointPos(body, p1[0],p1[1],p1[2], xp1);
+		dVector3 diff;
+		dSubtractVectors3( diff, xp1, xp0 );
+		dVector3 d;
+		dAddVectors3( d, xc, diff );
+		dsSetColor(0.2,0.7,0.2);
+		dsDrawLine( xc, d );
+
+		reader += sz;
+	}
+}
 
 
 static void simLoop(int pause)
@@ -168,21 +226,22 @@ static void simLoop(int pause)
 		dJointGroupEmpty (contactgroup);
 	}
 
-	dsSetColor (1,1,1);
 	// Draw the convex objects.
 	for ( int i=0; i<halton_numc; ++i )
 	{
+
 		dGeomID geom = hgeom[i];
 		if ( !geom ) continue;
+
+		draw_plane_normals( i );
+#if 1
 		dBodyID body = dGeomGetBody(geom);
-		//const dReal *pos = dBodyGetPosition(body);
-		//const dReal *rot = dBodyGetRotation(body);
 		const dReal *pos = dGeomGetPosition(geom);
 		const dReal *rot = dGeomGetRotation(geom);
 		if ( i==75 )
-			dsSetColor (1,1,0);
+			dsSetColorAlpha(1,1,0,0.39);
 		else
-			dsSetColor (1,1,1);
+			dsSetColorAlpha(1,1,1,0.39);
 		dsDrawConvex
 		(
 			pos, rot,
@@ -192,6 +251,7 @@ static void simLoop(int pause)
 			halton_numv[i],
 			halton_faces[i]
 		);
+#endif
 	}
 
 	if (drawpos)
@@ -202,6 +262,7 @@ static void simLoop(int pause)
 		for ( int i=0; i<halton_numc; ++i )
 		{
 			dBodyID body = hbody[i];
+			if ( !body ) continue;
 			const dReal *pos = dBodyGetPosition(body);
 			dReal x0[3] = { pos[0]-l, pos[1], pos[2] };
 			dReal x1[3] = { pos[0]+l, pos[1], pos[2] };
@@ -214,11 +275,65 @@ static void simLoop(int pause)
 			dsDrawLine(z0,z1);
 		}
 	}
+
+	//draw_plane_normals( 75 );
+}
+
+
+// Check to see if the vertices of the face actually form the same surface normal as was defined for the plane.
+static void sanity_check( int nr )
+{
+	const dReal* verts = halton_verts[nr];
+	const dReal* planes = halton_planes[nr];
+	const unsigned int* faces = halton_faces[nr];
+	const int numf = halton_numf[nr];
+
+	const unsigned int* reader = faces;
+	for ( int f=0; f<numf; ++f )
+	{
+		const dReal* plane = planes + f*4;
+		const int sz = *reader++;
+		fprintf( stdout, "Volume %d, face %d of size %d at dist %f\n", nr, f, sz, plane[3] );
+		for ( int t=0; t<sz-2; ++t )
+		{
+			const int i0 = reader[  0];
+			const int i1 = reader[t+1];
+			const int i2 = reader[t+2];
+			assert( i1 != i0 );
+			assert( i2 != i0 );
+			assert( i1 != i2 );
+			const dReal* v0 = verts + i0*3;
+			const dReal* v1 = verts + i1*3;
+			const dReal* v2 = verts + i2*3;
+			dVector3 u,v;
+		       	dSubtractVectors3( u, v1, v0 );
+			dSubtractVectors3( v, v2, v1 );
+			dVector3 cross;
+			dCalcVectorCross3( cross, u, v );
+			dNormalize3( cross );
+			const dReal dot = dCalcVectorDot3( plane, cross );
+			assert( dot >= 0.999f && dot <= 1.001f );
+			const dReal dist0 = dCalcVectorDot3( plane, v0 );
+			const dReal dist1 = dCalcVectorDot3( plane, v1 );
+			const dReal dist2 = dCalcVectorDot3( plane, v2 );
+			const dReal delta0 = dist0 - plane[3];
+			const dReal delta1 = dist1 - plane[3];
+			const dReal delta2 = dist2 - plane[3];
+			assert( fabs(delta0) < 0.0001 );
+			assert( fabs(delta1) < 0.0001 );
+			assert( fabs(delta2) < 0.0001 );
+		}
+		reader += sz;
+	}
 }
 
 
 int main (int argc, char **argv)
 {
+#if defined(linux)
+	feenableexcept( FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW );
+#endif
+
 	dMass m;
 
 	// setup pointers to drawstuff callback functions
@@ -259,6 +374,8 @@ int main (int argc, char **argv)
 
 	for (int i=0; i<halton_numc; ++i )
 	{
+		//if ( i!= 75 ) continue;
+		sanity_check( i );
 		dGeomID geom = dCreateConvex
 		(
 			space,
@@ -292,6 +409,7 @@ int main (int argc, char **argv)
 			dBodySetLinearDamping (body, 0.0005);
 			dBodySetAngularDamping(body, 0.0300);
 #endif
+
 			dGeomSetBody(geom,body);
 		}
 	}
